bf97fa2c0e26670256c159033c947afb6976cbb3
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-01-06 07:03:39 -0300

    add: crud compledo das habilidades do usuário
    add: página ProfileSkills
    add: componente CardPerfilSkill
    add: componente ModalAdicionarPerfilSkill
    add: componente ModalEditarPerfilSkill
    add: schema profileSkillSchema para validação
    add: dependencia react-native-dropdown-picker para fazer o dropdown das habilidades

---
+++ src/components/modalEditarPerfilSkill/index.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Modal, ActivityIndicator, TextInput, Alert } from 'react-native';
+import { styles } from './style';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { Entypo, MaterialIcons, AntDesign } from '@expo/vector-icons';
+import { skillSchema } from "../../schemas/skillSchema";
+import api from "../../service/api/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+interface ModalEditarPerfilSkillProps {
+	perfilSkill: profileSkillType;
+	isModalVisible: boolean;
+	triggerEdit: boolean;
+	setTriggerEdit: (value: boolean) => void;
+	setIsModalVisible: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+export const ModalEditarPerfilSkill = ({
+	perfilSkill,
+	isModalVisible,
+	triggerEdit,
+	setIsModalVisible,
+	setTriggerEdit
+}: ModalEditarPerfilSkillProps) => {
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	const {
+		register,
+		handleSubmit,
+		control,
+		watch,
+		formState: { errors }
+	} = useForm<profileSkillType>({
+		resolver: yupResolver(skillSchema)
+	});
+
+	const onSubmit: SubmitHandler<profileSkillType> = data => {
+		if (data.perfilSkillVersao == null || data.perfilSkillVersao === '') {
+			data.perfilSkillVersao = perfilSkill.perfilSkillVersao;
+		}
+		console.log('teste')
+		editSkill(data, perfilSkill.perfilSkillId);
+	};
+
+	const editSkill = async (data: profileSkillType, perfilSkillId: number) => {
+		const token = await AsyncStorage.getItem('id');
+		try {
+			const response = await api.put(`/api/perfilskills/${perfilSkillId}`, {
+				perfilSkillVersao: data.perfilSkillVersao,
+			}, {
+				headers: {
+					Authorization: `${token}`
+				}
+			});
+			console.log('Response data:', response.data);
+			Alert.alert('Habilidade editada com sucesso!');
+			setIsModalVisible(false);
+			setTriggerEdit(!triggerEdit);
+		} catch (err: any) {
+			console.log(typeof err);
+			alert(err.response.data.message);
+		}
+	};
+
+	return (
+		<>
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={isModalVisible}
+				onRequestClose={() => {
+					setIsModalVisible(false);
+				}}
+			>
+				<TouchableOpacity style={styles.modal} onPressOut={() => setIsModalVisible(false)}>
+					<TouchableOpacity activeOpacity={1} onPress={() => { }} style={styles.modalContainer}>
+						{
+							<>
+								{/* Modal de Textos */}
+								<View style={styles.textContainer}>
+									<View style={{ justifyContent: "center" }}>
+										<Text style={styles.title}>Editar Habilidade</Text>
+									</View>
+									<View style={{ marginBottom: 3 }}>
+										<Text style={styles.label}>Versão</Text>
+										{errors.perfilSkillVersao && (
+											<Text style={styles.errorLabel}>
+												{errors.perfilSkillVersao.message}
+											</Text>
+										)}
+										<View style={styles.inputContainer}>
+											<Controller
+												control={control}
+												rules={{
+													required: true,
+												}}
+												render={({ field: { onChange, onBlur, value } }) => (
+													<TextInput
+														onBlur={onBlur}
+														onChangeText={onChange}
+														value={value}
+														style={styles.title}
+													>
+													</TextInput>
+												)}
+												name="perfilSkillVersao"
+												defaultValue={perfilSkill.perfilSkillVersao}
+											/>
+										</View>
+									</View>
+								</View>
+								<TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.addSkillButton}>
+									<Text style={styles.addSkillButtonText}>Editar</Text>
+								</TouchableOpacity>
+							</>
+						}
+					</TouchableOpacity>
+				</TouchableOpacity>
+			</Modal>
+		</>
+	)
+}
